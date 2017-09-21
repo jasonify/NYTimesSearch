66dc2d216e75ed6e6f9880c6ef7483445b30af6b
jason <bjason@gmail.com>
2017-09-20 21:54:14 -0700

    Load url

--- app/src/main/java/com/example/jason/nytimessearch/activities/ArticleActivity.java
+++ app/src/main/java/com/example/jason/nytimessearch/activities/ArticleActivity.java
@@ -2,7 +2,9 @@ package com.example.jason.nytimessearch.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.jason.nytimessearch.R;
 
@@ -13,8 +15,17 @@ public class ArticleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
 
-        String url = getIntent().getStringExtra("url");
+        final String url = getIntent().getStringExtra("url");
         WebView webView = (WebView) findViewById(R.id.wvArticle);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+
+                view.loadUrl(url);
+                return true;
+            }
+        });
 
         webView.loadUrl(url);
     }
